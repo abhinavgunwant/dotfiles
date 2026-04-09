aaf5adbb0fa60aa197a59e90f2cf29543c12af4e
Abhinav Gunwant <abhigunwant@gmail.com>
2026-04-09 19:08:16 +0530

    window maximize on startup for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,17 +2,21 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+-- Font settings
 config.font_size = 16
-config.window_background_opacity = 0.95
-config.default_prog = { 'powershell' }
 config.font = wezterm.font {
     family = 'FiraCode Nerd Font',
     weight = 'Regular',
 }
+
+config.window_background_opacity = 0.95
+config.default_prog = { 'powershell' }
 config.window_frame = {
     active_titlebar_bg = '#222222',
 }
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+-- Menu on right click on '+' button on tab bar.
 config.launch_menu = {
     {
         label = 'patram - nvim',
@@ -24,6 +28,12 @@ config.launch_menu = {
         cwd = 'C:\\Users\\abhig\\Projects\\C++\\patram',
     },
 }
+
+-- Maxmize on startup.
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
 return config
 
